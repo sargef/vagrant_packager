9dbc329d6431022d5c512737a8324b3518d8e8a3
sargef <sargef38@bigpond.com>
2020-05-31 18:59:08 +0930

    adding files for LAMP stack

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update CentOs with any patches
+yum update -y --exclude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MYSQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+
+service httpd restart
